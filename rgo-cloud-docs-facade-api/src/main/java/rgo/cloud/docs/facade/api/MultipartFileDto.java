3f52e50be69fce6b4f115bc37ee6b89fbc8ae9ce
evgeny_mordyasov <michisig11i7@gmail.com>
2023-03-05 16:41:57 +0700

    exclude file data for toString

--- rgo-cloud-docs-facade-api/src/main/java/rgo/cloud/docs/facade/api/MultipartFileDto.java
+++ rgo-cloud-docs-facade-api/src/main/java/rgo/cloud/docs/facade/api/MultipartFileDto.java
@@ -11,7 +11,10 @@ public class MultipartFileDto {
     private final String fullFileName;
     private final String fileName;
     private final String extension;
+
+    @ToString.Exclude
     private final byte[] data;
+
     private final boolean isEmpty;
     private final long size;
 }
